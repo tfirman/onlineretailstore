727460ba14395f5bae4b36f352fc10466974ddca
Tim Firman <tkfirman@gmail.com>
2018-04-03 10:40:57 -0700

    adds more firebase functionality

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Album } from '../album.model';
 import { AlbumService } from '../album.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-album-detail',
@@ -11,21 +12,20 @@ import { AlbumService } from '../album.service';
   providers: [AlbumService]
 })
 export class AlbumDetailComponent implements OnInit {
-  albumId: number;
-  albumToDisplay: Album;
+  albumId: string;
+  albumToDisplay;
 
+  constructor(private route: ActivatedRoute, private location: Location, private albumService: AlbumService) { }
 
-  constructor(
-    private route: ActivatedRoute,
-    private location: Location,
-    private albumService: AlbumService
-  ) {}
-  
   ngOnInit() {
-    this.route.params.forEach((urlParameters) => {
-      this.albumId = parseInt(urlParameters['id']);
-    });
-    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
+    this.route.params.forEach((urlParametersArray) => {
+     this.albumId = urlParametersArray['id'];
+   });
+   this.albumService.getAlbumById(this.albumId).subscribe(dataLastEmittedFromObserver => {
+     this.albumToDisplay = new Album(dataLastEmittedFromObserver.title,
+                                      dataLastEmittedFromObserver.artist,
+                                      dataLastEmittedFromObserver.description)
+   })
   }
 
 }
